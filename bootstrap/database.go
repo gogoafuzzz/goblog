c53031c739a7d9ac68f013ba6410b65574c7501e
Ryan <gogo.afuzzz@gmail.com>
2022-05-15 14:08:58 +0800

    Database initialized

---
+++ bootstrap/database.go
@@ -0,0 +1,43 @@
+package bootstrap
+
+import (
+	"errors"
+	"fmt"
+	"goblog/pkg/config"
+	"goblog/pkg/database"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func SetupDB() {
+	var dbConfig gorm.Dialector
+	switch config.Get("database.connection") {
+	case "mysql":
+		dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=%v&parseTime=True&multiStatements=True&loc=Local",
+			config.Get("database.mysql.username"),
+			config.Get("database.mysql.password"),
+			config.Get("database.mysql.host"),
+			config.Get("database.mysql.port"),
+			config.Get("database.mysql.database"),
+			config.Get("database.mysql.charset"),
+		)
+
+		dbConfig = mysql.New(mysql.Config{
+			DSN: dsn,
+		})
+	case "sqlite":
+		database := config.Get("database.sqlite.database")
+		dbConfig = sqlite.Open(database)
+	default:
+		panic(errors.New("database connection not supported"))
+	}
+
+	database.Connect(dbConfig, logger.Default.LogMode(logger.Info))
+	database.SQLDB.SetMaxIdleConns(config.GetInt("database.max_idle_connections"))
+	database.SQLDB.SetMaxOpenConns(config.GetInt("database.max_open_connections"))
+	database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.max_connection_lifetime")) * time.Second)
+}
--- main.go
+++ main.go
@@ -20,9 +20,11 @@ func main() {
 	flag.StringVar(&env, "env", "", "加載 .env 檔案，如 --env=testing 加載的是 .env.testing 檔案")
 	flag.Parse()
 	config.InitConfig(env)
-
 	router := gin.New()
+
+	bootstrap.SetupDB()
 	bootstrap.SetupRoute(router)
+
 	err := router.Run(":" + config.Get("app.port"))
 
 	if err != nil {
